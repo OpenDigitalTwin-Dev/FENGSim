b8331416c0ba776d66c9aa7f167587f59dcd85f5
fengsim <jiping.xin@lsec.cc.ac.cn>
2022-06-02 10:46:51 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('exts'))
+sys.path.append(os.path.abspath('ext/breathe'))
 
 # -- General configuration -----------------------------------------------------
 
