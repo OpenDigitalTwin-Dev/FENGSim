ae7557e3ed718d70c8db7f6fa0a6902ea36a0ade
fengsim <jiping.xin@lsec.cc.ac.cn>
2022-06-15 10:42:31 +0800

    test

--- docs/conf.py
+++ docs/conf.py
@@ -358,7 +358,7 @@ exhale_args = {
     "rootFileName":          "library_root.rst",
     "doxygenStripFromPath":  "..",
     # Heavily encouraged optional argument (see docs)
-    "rootFileTitle":         "ALE",
+    "rootFileTitle":         "ODT",
     # Suggested optional arguments
     "createTreeView":        True,
     # TIP: if using the sphinx-bootstrap-theme, you need
