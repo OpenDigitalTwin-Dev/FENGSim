2853a822bc3f792f583931759bc6afa08794dc04
fengsim <jiping.xin@lsec.cc.ac.cn>
2023-08-13 12:13:35 +0800

    cgal interfaces

---
+++ starters/CGAL/src/CGALInterface.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <vector>
+
+// read points from point_cloud_2.cin and export mesh to vtk files
+void CGAL_IncrementalTriangulation_2 ();
+void CGAL_DelaunayTriangulation_2();
+void CGAL_ConstrainedDelaunayTriangulation_2();
+void CGAL_MeshGeneration_2();
+
+// read points from point_cloud_3.cin and export mesh to vtk files
+void CGAL_Triangulation_3();
+void CGAL_DelaunayTriangulation_3();
+// read polyhedron from point_cloud_3.off and export mesh to vtk files
+void CGAL_MeshGeneration_3 ();
--- starters/CGAL/src/Main.C
+++ starters/CGAL/src/Main.C
@@ -1,9 +1,18 @@
 // file:   Main.C
 // author: Jiping Xin
 
-#include "SurfaceReconstruction.h"
+#include "CGALInterface.h"
 
 int main (int argv, char** argc) {
-    CGALMeshGeneration();
+
+    CGAL_IncrementalTriangulation_2();
+    CGAL_DelaunayTriangulation_2();
+    CGAL_ConstrainedDelaunayTriangulation_2();
+    CGAL_MeshGeneration_2();
+
+    CGAL_Triangulation_3();
+    CGAL_DelaunayTriangulation_3();
+    CGAL_MeshGeneration_3 ();
+    
     return 0;
 }
