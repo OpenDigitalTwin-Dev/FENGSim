df32d731d3ed4074fb85782531b371b50518c06d
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-03-20 18:07:06 +0800

    c

--- cli/install.sh
+++ cli/install.sh
@@ -59,7 +59,7 @@ make install
 cd ../../gmsh
 mkdir build
 cd build
-cmake .. -DOCC_INC=$PWD/../../oce_install/include/oce -DCMAKE_INSTALL_PREFIX=$PWD/../../gmsh_install -DENABLE_BUILD_DYNAMIC=ON -DENABLE_BUILD_SHARED=ON -DENABLE_PETSC=OFF -DENABLE_FLTK=OFF -DENABLE_MMG3D=OFF 
+cmake .. -DOCC_INC=$PWD/../../oce_install/include/oce -DCMAKE_INSTALL_PREFIX=$PWD/../../gmsh_install -DENABLE_BUILD_DYNAMIC=ON -DENABLE_BUILD_SHARED=ON -DENABLE_PETSC=OFF -DENABLE_FLTK=OFF -DENABLE_MMG3D=OFF -DENABLE_MED=OFF 
 make -j4
 make install
 
