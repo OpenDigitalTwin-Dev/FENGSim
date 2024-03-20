f183a0379fbe6b80aa721c3430d6c97f04ec01ea
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-03-20 11:12:20 +0800

    c

--- cli/qtcreator.sh
+++ cli/qtcreator.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 LD_LIBRARY_PATH=$PWD/../tools/qt/icu/usr/local/lib
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../tools/qt/icu/usr/local/lib
-_ORIGINAL_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../tools/qt/icu/usr/local/lib
+#_ORIGINAL_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../tools/qt/5.12.12/plugins
 export LD_LIBRARY_PATH
 
