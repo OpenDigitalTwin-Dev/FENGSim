17fc0239fc0d9914a96e8c9125195750dd5c78f8
fengsim <jiping.xin@.lsec.cc.ac.cn>
2024-04-09 16:23:09 +0800

    c

---
+++ tools/libarcus/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt -y install python3.10-dev
+sudo apt -y install python3-sip-dev
+
+rm -rf build
+mkdir build
+cd build
+cmake .. \
+      -D CMAKE_INSTALL_PREFIX=$PWD/../../libarcus_install \
+      -D Protobuf_INCLUDE_DIR=$PWD/../../protobuf_install/include/ \
+      -D Protobuf_LIBRARY_DEBUG=$PWD/../../protobuf_install/lib/libprotobuf.so \
+      -D Protobuf_LIBRARY_RELEASE=$PWD/../../protobuf_install/lib/libprotobuf.so \
+      -D Protobuf_LITE_LIBRARY_DEBUG=$PWD/../../protobuf_install/lib/libprotobuf-lite.so \
+      -D Protobuf_LITE_LIBRARY_RELEASE=$PWD/../../protobuf_install/lib/libprotobuf-lite.so \
+      -D Protobuf_PROTOC_EXECUTABLE=$PWD/../../protobuf_install/bin/protoc \
+      -D Protobuf_PROTOC_LIBRARY_DEBUG=$PWD/../../protobuf_install/lib/libprotoc.so \
+      -D Protobuf_PROTOC_LIBRARY_RELEAS=$PWD/../../protobuf_install/lib/libprotoc.so
+
+make -j4
+make install
+
+
