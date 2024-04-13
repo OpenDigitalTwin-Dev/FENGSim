f8f8f3109fa2df33ef8a2c329fe00a643fe03bbc
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-04-13 14:40:48 +0800

    c

--- tools/netgen/install.sh
+++ tools/netgen/install.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+apt update
+apt -y install cmake
+apt -y install make
+apt -y install gcc g++
+apt -y install git
+apt -y install zlib1g-dev
+#apt -y install tcl-dev
+#apt -y install tk-dev
+
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../netgen_install -DUSE_OCC=OFF -DUSE_PYTHON=OFF
+cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../netgen_install -DUSE_OCC=OFF -DUSE_PYTHON=OFF -DUSE_GUI=OFF
 make -j4
 make install
