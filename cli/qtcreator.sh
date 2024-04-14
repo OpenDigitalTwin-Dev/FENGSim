c575518db562c338479458696539d18642586e78
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-04-14 15:12:54 +0800

    c

--- cli/qtcreator.sh
+++ cli/qtcreator.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=$PWD/../tools/qt/icu/usr/local/lib
-#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../tools/qt/icu/usr/local/lib
+LD_LIBRARY_PATH=$PWD/../toolkit/qt/icu/usr/local/lib
+#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../toolkit/qt/icu/usr/local/lib
 #_ORIGINAL_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../tools/qt/5.12.12/plugins
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../toolkit/qt/5.12.12/plugins
 export LD_LIBRARY_PATH
 
-./../tools/qt/qtcreator/bin/qtcreator
+./../toolkit/qt/qtcreator/bin/qtcreator
