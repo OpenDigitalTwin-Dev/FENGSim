5e5a36e7625bee306e589904dd11345649e3b283
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-04-25 17:02:17 +0800

    c

--- toolkit/netgen/install.sh
+++ toolkit/netgen/install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-apt update
-apt -y install cmake
-apt -y install make
-apt -y install gcc g++
-apt -y install git
-apt -y install zlib1g-dev
-#apt -y install tcl-dev
-#apt -y install tk-dev
+sudo apt update
+sudo apt -y install cmake
+sudo apt -y install make
+sudo apt -y install gcc g++
+sudo apt -y install git
+sudo apt -y install zlib1g-dev
+#sudo apt -y install tcl-dev
+#sudo apt -y install tk-dev
 
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../netgen_install -DUSE_OCC=OFF -DUSE_PYTHON=OFF -DUSE_GUI=OFF
+cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../install/netgen_install -DUSE_OCC=OFF -DUSE_PYTHON=OFF -DUSE_GUI=OFF
 make -j4
 make install
--- toolkit/netgen/uninstall.sh
+++ toolkit/netgen/uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -rf build
-rm -rf ../netgen_install
+rm -rf ../install/netgen_install
