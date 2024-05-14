ec93a3c66fb8bea1be05a5544374e03cad98d4cd
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-05-14 10:32:52 +0800

    c

--- starter/Mesh/src/ng_stl.cpp
+++ starter/Mesh/src/ng_stl.cpp
@@ -16,12 +16,15 @@
 
 #include <iostream>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
 
 namespace nglib {
 #include <nglib.h>
 }
+
+void tovtk (string file_name);
 
 int main (int argc, char ** argv)
 {
@@ -112,17 +115,76 @@ int main (int argc, char ** argv)
    cout << "Saving Mesh in VOL Format...." << endl;
    Ng_SaveMesh(mesh,"test.vol");
 
+   tovtk("test");
 
    // refinement without geometry adaption:
-   // Ng_Uniform_Refinement (mesh);
+   Ng_Uniform_Refinement (mesh);
 
    // refinement with geometry adaption:   
-   Ng_STL_Uniform_Refinement (stl_geom, mesh);
+   //Ng_STL_Uniform_Refinement (stl_geom, mesh);
 
    cout << "elements after refinement: " << Ng_GetNE(mesh) << endl;
    cout << "points   after refinement: " << Ng_GetNP(mesh) << endl;
 
    Ng_SaveMesh(mesh,"test_ref.vol");
 
+   tovtk("test_ref");
+
    return 0;
+}
+
+
+void tovtk (string file_name) {    
+    std::ifstream is(file_name+".vol");
+    std::ofstream os(file_name+".vtk");
+    os << "# vtk DataFile Version 2.0" << std::endl;
+    os << "Unstructured Grid Example" << std::endl;
+    os << "ASCII" << std::endl;
+    os << "DATASET UNSTRUCTURED_GRID" << std::endl;
+    const int len = 256;
+    char L[len];
+    while (strncasecmp("points",L,6)) {
+	is.getline(L,len);
+    }
+    is.getline(L,len);
+    int n1;
+    sscanf(L,"%d",&n1);
+    os << "POINTS " << n1 <<" float" << std::endl;
+    std::cout << n1 << std::endl;
+    for (int i=0; i<n1; i++) {
+	is.getline(L,len);
+	double z[3];
+	sscanf(L,"%lf %lf %lf",z,z+1,z+2);
+	os << z[0] << " "
+	   << z[1] << " "
+	   << z[2] << std::endl;
+	
+    }
+    is.close();
+    is.open(file_name+".vol");
+    while (strncasecmp("volumeelements",L,14)) {
+	is.getline(L,len);
+    }
+    is.getline(L,len);
+    int n2;
+    sscanf(L,"%d",&n2);
+    std::cout << n2 << std::endl;
+    os << "CELLS " << n2 << " " << n2*5 << std::endl;
+    for (int i=0; i<n2; i++) {
+	is.getline(L,len);
+	double z[4];
+	sscanf(L,"%*lf %*lf %lf %lf %lf %lf",z,z+1,z+2,z+3);
+	os << 4 << " "
+	   << z[0]-1 << " "
+	   << z[1]-1 << " "
+	   << z[2]-1 << " "
+	   << z[3]-1 << std::endl;
+    }
+    is.close();
+
+    os << "CELL_TYPES " << n2 << std::endl;
+    for (int i=0; i<n2; i++) {
+	os << 10 << std::endl;
+    }
+    os.close();
 }
