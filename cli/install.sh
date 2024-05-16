506899c37e5ccb64e7d47a2a2a8e4b2bc919a1eb
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-05-16 14:43:14 +0800

    c

--- cli/install.sh
+++ cli/install.sh
@@ -85,3 +85,8 @@ cd build
 cmake ..
 make
 
+cd ../../../cli
+LD_LIBRARY_PATH=$PWD/../toolkit/qt/icu/usr/local/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../toolkit/qt/5.12.12/plugins
+export LD_LIBRARY_PATH
+./../toolkit/qt/qtcreator/bin/qtcreator ./../starter/FENGSim/FENGSim.pro
