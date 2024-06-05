a6f17749ce0b988513a4d073e03f3a8d492e4a5e
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-06-05 15:24:23 +0800

    c

--- cli/qtcreator.sh
+++ cli/qtcreator.sh
@@ -5,5 +5,4 @@ LD_LIBRARY_PATH=$PWD/../toolkit/qt/icu/usr/local/lib
 #_ORIGINAL_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../toolkit/qt/5.12.12/plugins
 export LD_LIBRARY_PATH
-
 ./../toolkit/qt/qtcreator/bin/qtcreator ./../starter/FENGSim/FENGSim.pro
