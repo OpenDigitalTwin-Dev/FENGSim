5a1a5bdb2855bb5f184af4ff69eb80c368abd27d
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-06-14 13:07:58 +0800

    c

--- toolkit/DocsGPT/application/utils.py
+++
@@ -1,7 +0,0 @@
-from transformers import GPT2TokenizerFast
-
-#tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
-#tokenizer.model_max_length = 100000
-def count_tokens(string):
-    #return len(tokenizer(string)['input_ids'])
-    return 0
