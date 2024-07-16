992b23283320c580db4bd84c81a61ec6dedfdbc3
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-07-16 11:34:07 +0800

    c

--- toolkit/json/install.sh
+++ toolkit/json/install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+sudo apt update
+sudo apt -y install cmake make gcc g++
+
 rm -rf build
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../install/json_install
-make
+make -j4
 make install
