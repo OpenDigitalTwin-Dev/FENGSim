5803a889f07a0425cd84aca65670bf2ffb4c2d9e
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-07-22 15:22:28 +0800

    c

--- toolkit/r3d/deprecated/examples/voxelize.c
+++ toolkit/r3d/deprecated/examples/voxelize.c
@@ -103,6 +103,81 @@ int main() {
 		}
 	    }
 
+    int n_cell = 0;
+    for(i = 0; i < vox_grid.n.i; ++i) 
+	for(j = 0; j < vox_grid.n.j; ++j)
+	    for(k = 0; k < vox_grid.n.k; ++k) {
+		if (vox_grid.moments[0][vox_ind(i, j, k)]>0)
+		    //printf("%f",vox_grid.moments[0][vox_ind(i, j, k)]);
+		    n_cell++;
+	    }
+    
+    
+    FILE *fp;
+    fp = fopen("vox_example.vtk","w");
+    fprintf(fp, "# vtk DataFile Version 2.0\n");
+    fprintf(fp, "Structured Grid by Portage\n");
+    fprintf(fp, "ASCII\n");
+    fprintf(fp, "DATASET UNSTRUCTURED_GRID\n");
+    fprintf(fp, "POINTS %d float\n", n_cell*8);
+    for(i = 0; i < vox_grid.n.i; ++i) {
+	for(j = 0; j < vox_grid.n.j; ++j) {
+	    for(k = 0; k < vox_grid.n.k; ++k) {
+		if (vox_grid.moments[0][vox_ind(i, j, k)]>0) {
+		    double p1[3];
+		    p1[0] = 0 + i*vox_grid.d.x;
+		    p1[1] = 0 + j*vox_grid.d.y;
+		    p1[2] = 0 + k*vox_grid.d.z;
+		    double p2[3];
+		    p2[0] = vox_grid.d.x + i*vox_grid.d.x;
+		    p2[1] = 0 + j*vox_grid.d.y;
+		    p2[2] = 0 + k*vox_grid.d.z;
+		    double p3[3];
+		    p3[0] = 0 + i*vox_grid.d.x;
+		    p3[1] = vox_grid.d.y + j*vox_grid.d.y;
+		    p3[2] = 0 + k*vox_grid.d.z;
+		    double p4[3];
+		    p4[0] = vox_grid.d.x + i*vox_grid.d.x;
+		    p4[1] = vox_grid.d.y + j*vox_grid.d.y;
+		    p4[2] = 0 + k*vox_grid.d.z;
+		    double p5[3];
+		    p5[0] = 0 + i*vox_grid.d.x;
+		    p5[1] = 0 + j*vox_grid.d.y;
+		    p5[2] = vox_grid.d.z + k*vox_grid.d.z;
+		    double p6[3];
+		    p6[0] = vox_grid.d.x + i*vox_grid.d.x;
+		    p6[1] = 0 + j*vox_grid.d.y;
+		    p6[2] = vox_grid.d.z + k*vox_grid.d.z;
+		    double p7[3];
+		    p7[0] = 0 + i*vox_grid.d.x;
+		    p7[1] = vox_grid.d.y + j*vox_grid.d.y;
+		    p7[2] = vox_grid.d.z + k*vox_grid.d.z;
+		    double p8[3];
+		    p8[0] = vox_grid.d.x + i*vox_grid.d.x;
+		    p8[1] = vox_grid.d.y + j*vox_grid.d.y;
+		    p8[2] = vox_grid.d.z + k*vox_grid.d.z;
+		    fprintf(fp, "%f %f %f\n", p1[0], p1[1], p1[2]);
+		    fprintf(fp, "%f %f %f\n", p2[0], p2[1], p2[2]);
+		    fprintf(fp, "%f %f %f\n", p3[0], p3[1], p3[2]);
+		    fprintf(fp, "%f %f %f\n", p4[0], p4[1], p4[2]);
+		    fprintf(fp, "%f %f %f\n", p5[0], p5[1], p5[2]);
+		    fprintf(fp, "%f %f %f\n", p6[0], p6[1], p6[2]);
+		    fprintf(fp, "%f %f %f\n", p7[0], p7[1], p7[2]);
+		    fprintf(fp, "%f %f %f\n", p8[0], p8[1], p8[2]);
+		}   
+	    }
+	}
+    }
+    
+    fprintf(fp, "CELLS %d %d\n", n_cell, n_cell*9);
+    for (int i=0; i<n_cell; i++) {
+        fprintf(fp, "8 %d %d %d %d %d %d %d %d\n", 0+i*8, 1+i*8, 2+i*8, 3+i*8, 4+i*8, 5+i*8, 6+i*8, 7+i*8);
+    }
+    fprintf(fp, "CELL_TYPES %d\n", n_cell);
+    for (int i=0; i<n_cell; i++) {
+        fprintf(fp, "11\n"); 
+    }
+    fclose(fp);
     
     printf("Moments (sum over voxels):\n");
     printf("  Integral[ 1 dV ] = %f\n",   moments_vox[0]);
