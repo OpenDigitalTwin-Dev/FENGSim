b831b2429e8753205f9185945006534325d9d7d2
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-08-05 13:36:24 +0800

    c

--- toolkit/parmetis/install.sh
+++ toolkit/parmetis/install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#chang the following in Makefile
+#gklib_path = ../NSM/extern/ALE/install/gk
+#metis_path = ../NSM/extern/ALE/install/metis
+
 make config prefix=$PWD/../install/parmetis_install
 make -j4
 make install
