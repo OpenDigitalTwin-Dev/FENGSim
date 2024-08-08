f7deaa48df4d05b265457fdf9adf3cba7452b62e
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-08-08 14:56:43 +0800

    c

--- toolkit/parmetis/install.sh
+++ toolkit/parmetis/install.sh
@@ -4,6 +4,9 @@
 #gklib_path = ../NSM/extern/ALE/install/gk
 #metis_path = ../NSM/extern/ALE/install/metis
 
+sudo apt update
+sudo apt -y install make gcc g++ cmake libmpich-dev
+
 make config prefix=$PWD/../install/parmetis_install
 make -j4
 make install
