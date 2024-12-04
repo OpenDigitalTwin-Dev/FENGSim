168966a43e0160f4343b55c2540d76caf2a68c44
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-12-04 18:36:22 +0800

    c

--- starter/WebCAD/install.sh
+++ starter/WebCAD/install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 #sftp jiping@47.242.114.253:/home/jiping/ubuntu_apache2.tar
-sudo docker stop webgl
-sudo docker rm webgl
-sudo docker rmi ubuntu/apache2
 sudo docker load --input ubuntu_apache2.tar
 
 #sudo docker run -p 80:80 -v ./:/usr/local/apache2/htdocs/ --name webgl -d httpd
--- starter/WebCAD/uninstall.sh
+++ starter/WebCAD/uninstall.sh
@@ -2,5 +2,7 @@
 
 sudo docker stop webgl
 sudo docker rm webgl
+sudo docker rmi ubuntu/apache2
+
 sudo rm -rf build
 sudo rm -rf oce-OCE-0.18
