436d0adcf1d443bf6883b2fb48c2445bfbd28082
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-03-15 17:14:24 +0800

    c

--- toolkit/Geometry/vtk/install.sh
+++ toolkit/Geometry/vtk/install.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd build
-cmake .. -DQt5_DIR=$PWD/../../qt/5.12.12/lib/cmake/Qt5 -DCMAKE_INSTALL_PREFIX=$PWD/../../install/vtk_install
+cmake .. -DQt5_DIR=$PWD/../../../qt/5.12.12/lib/cmake/Qt5 -DCMAKE_INSTALL_PREFIX=$PWD/../../install/vtk_install
 make -j4
 make install
