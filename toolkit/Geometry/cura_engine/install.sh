c17694452a454e55774bef7583aa7b36a87cd1e6
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-03-17 11:25:34 +0800

    c

--- toolkit/Geometry/cura_engine/install.sh
+++ toolkit/Geometry/cura_engine/install.sh
@@ -8,14 +8,14 @@ sudo apt -y install libstb-dev
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../install/cura_engine_install \
-	  -D Arcus_DIR=$PWD/../../install/libarcus_install/lib/cmake/Arcus/ \
-      -D Protobuf_INCLUDE_DIR=$PWD/../../install/protobuf_install/include/ \
-      -D Protobuf_LIBRARY_DEBUG=$PWD/../../install/protobuf_install/lib/libprotobuf.so \
-      -D Protobuf_LIBRARY_RELEASE=$PWD/../../install/protobuf_install/lib/libprotobuf.so \
-      -D Protobuf_LITE_LIBRARY_DEBUG=$PWD/../../install/protobuf_install/lib/libprotobuf-lite.so \
-      -D Protobuf_LITE_LIBRARY_RELEASE=$PWD/../../install/protobuf_install/lib/libprotobuf-lite.so \
-      -D Protobuf_PROTOC_EXECUTABLE=$PWD/../../install/protobuf_install/bin/protoc \
-      -D Protobuf_PROTOC_LIBRARY_DEBUG=$PWD/../../install/protobuf_install/lib/libprotoc.so \
-      -D Protobuf_PROTOC_LIBRARY_RELEAS=$PWD/../../install/protobuf_install/lib/libprotoc.so
+	  -D Arcus_DIR=$PWD/../../../Tools/install/libarcus_install/lib/cmake/Arcus/ \
+      -D Protobuf_INCLUDE_DIR=$PWD/../../../Tools/install/protobuf_install/include/ \
+      -D Protobuf_LIBRARY_DEBUG=$PWD/../../../Tools/install/protobuf_install/lib/libprotobuf.so \
+      -D Protobuf_LIBRARY_RELEASE=$PWD/../../../Tools/install/protobuf_install/lib/libprotobuf.so \
+      -D Protobuf_LITE_LIBRARY_DEBUG=$PWD/../../../Tools/install/protobuf_install/lib/libprotobuf-lite.so \
+      -D Protobuf_LITE_LIBRARY_RELEASE=$PWD/../../../Tools/install/protobuf_install/lib/libprotobuf-lite.so \
+      -D Protobuf_PROTOC_EXECUTABLE=$PWD/../../../Tools/install/protobuf_install/bin/protoc \
+      -D Protobuf_PROTOC_LIBRARY_DEBUG=$PWD/../../../Tools/install/protobuf_install/lib/libprotoc.so \
+      -D Protobuf_PROTOC_LIBRARY_RELEAS=$PWD/../../../Tools/install/protobuf_install/lib/libprotoc.so
 make -j4
 make install
