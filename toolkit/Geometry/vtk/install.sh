77de2966a35c68075fe218d5f3e3773122c8e187
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-03-17 08:53:13 +0800

    c

--- toolkit/Geometry/vtk/install.sh
+++ toolkit/Geometry/vtk/install.sh
@@ -6,6 +6,6 @@ sudo apt install gcc-9 g++-9 gcc-10 g++-10
 
 mkdir build
 cd build
-cmake .. -DQt5_DIR=$PWD/../../../qt/5.12.12/lib/cmake/Qt5 -DCMAKE_INSTALL_PREFIX=$PWD/../../install/vtk_install -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_C_COMPILER=gcc-9
+cmake .. -DQt5_DIR=$PWD/../../../Tools/qt/5.12.12/lib/cmake/Qt5 -DCMAKE_INSTALL_PREFIX=$PWD/../../install/vtk_install -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_C_COMPILER=gcc-9
 make -j4
 make install
