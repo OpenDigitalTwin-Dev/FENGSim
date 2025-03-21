80c9388d77a1be3990b53c7f4c4ca7095f6c22f4
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-03-21 09:19:51 +0800

    c

---
+++ cli/test-docker.sh
@@ -0,0 +1,4 @@
+sudo docker rm test
+sudo docker run --name test -it -v $HOME/FENGSim:/test ubuntu:24.04 /bin/bash
+#sudo docker run --name test -it -v $HOME/Downloads:/test ubuntu:22.04 /bin/bash
+
