22e3552aef2607e6b5385958a6ef455cc555feff
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-04-04 12:52:12 +0800

    c

--- cli/test-docker.sh
+++ cli/test-docker.sh
@@ -1,4 +1,5 @@
 sudo docker rm test
 sudo docker run --name test -it -v $HOME/FENGSim:/test ubuntu:24.04 /bin/bash
-#sudo docker run --name test -it -v $HOME/Downloads:/test ubuntu:22.04 /bin/bash
+#sudo docker run --name test -it -v $HOME/Downloads:/test ubuntu:24.04 /bin/bash
+#sudo docker run --name test -it -v $HOME/Downloads:/test ros2-ubuntu22.04 /bin/bash
 
