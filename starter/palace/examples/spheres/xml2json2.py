6bfe2f6668c687264b6ea40004525eceb0e0324e
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-04-14 13:44:38 +0800

    c

--- starter/palace/examples/spheres/xml2json2.py
+++ starter/palace/examples/spheres/xml2json2.py
@@ -2,33 +2,34 @@ import xml.etree.ElementTree as E
 import sys
 import json
 
-tree = E.parse('test3.xml')
+tree = E.parse('configure.xml')
 root = tree.getroot()
 
 print(root)
 
 d={}
-
 for child in root:
-    if child.tag == "Problem" :
-        d[child.tag] = {'Type': child.text, 'Verbose': 2, 'Output': 'postpro'}
-    if child.tag == "Materials" :
-        d2=[]
-        for child2 in child: 
-            d3 ={}
-            for child3 in child2:
-                d3[child3.tag] = child3.text
-            d2.append(d3)
-        d[child.tag] = d2
-    if child.tag == "Boundaries" :
-        d2 = {}
-        for child2 in child:
-            d3 = {}
-            for child3 in child2:
-                for child4 in child3:
-                    d3[child4.tag] = child4.text
-            d2[child3.tag] = d3
-        d[child.tag] = d2
+    if child.tag == "Electromagnetism" :
+        for child1 in child:
+            if child1.tag == "Problem" :
+                d[child1.tag] = {'Type': child1.text, 'Verbose': 2, 'Output': 'postpro'}
+            if child1.tag == "Materials" :
+                d2=[]
+                for child2 in child1: 
+                    d3 ={}
+                    for child3 in child2:
+                        d3[child3.tag] = child3.text
+                    d2.append(d3)
+                d[child1.tag] = d2
+            if child1.tag == "Boundaries" :
+                d2 = {}
+                for child2 in child1:
+                    d3 = {}
+                    for child3 in child2:
+                        for child4 in child3:
+                            d3[child4.tag] = child4.text
+                    d2[child3.tag] = d3
+                d[child1.tag] = d2
     
 
 
