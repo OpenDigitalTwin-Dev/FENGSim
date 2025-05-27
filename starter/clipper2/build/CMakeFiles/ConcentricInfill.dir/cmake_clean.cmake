59956d2c666608afb6c0b6806e166521ad499f7c
“maning103” <maning103@sina.com>
2025-05-27 23:31:40 +0800

    build(clipper2): 添加 Clipper2Utils 配置
    - 在 clipper2/CPP 目录下添加 Clipper2Utils 的配置文件和目标导出
    - 更新 CMakeLists.txt 文件以支持新功能

---
+++ starter/clipper2/build/CMakeFiles/ConcentricInfill.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConcentricInfill.dir/src/ConcentricInfill.cpp.o"
+  "CMakeFiles/ConcentricInfill.dir/src/ConcentricInfill.cpp.o.d"
+  "ConcentricInfill"
+  "ConcentricInfill.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ConcentricInfill.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ starter/clipper2/build/CMakeFiles/ConcentricInfill.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ConcentricInfill.
+# This may be replaced when dependencies are built.
