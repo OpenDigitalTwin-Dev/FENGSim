ef31282e60e2be8393a614ac505431c5f7696047
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-05-28 12:10:53 +0800

    c

--- toolkit/Geometry/gmsh/install.sh
+++ toolkit/Geometry/gmsh/install.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd build
-cmake .. -DOCC_INC=$PWD/../../install/oce_install/include/oce -DCMAKE_INSTALL_PREFIX=$PWD/../../install/gmsh_install -DENABLE_BUILD_DYNAMIC=ON -DENABLE_BUILD_SHARED=ON -DENABLE_PETSC=OFF -DENABLE_FLTK=OFF -DENABLE_MMG3D=OFF -DENABLE_MED=OFF 
+cmake .. -DOCC_INC=$PWD/../../install/oce_install/include/oce -DCMAKE_INSTALL_PREFIX=$PWD/../../install/gmsh_install -DENABLE_BUILD_DYNAMIC=ON -DENABLE_BUILD_SHARED=ON -DENABLE_BUILD_LIB=ON -DENABLE_PETSC=OFF -DENABLE_FLTK=OFF -DENABLE_MMG3D=OFF -DENABLE_MED=OFF 
 make -j4
 make install
