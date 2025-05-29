e84a4565af3d48ae8cc461b047cbb59f23306d3f
“maning103” <maning103@sina.com>
2025-05-29 13:20:57 +0800

    docker install  .sh file modify clipper utils install  modify

--- cli/test-docker-gui.sh
+++ cli/test-docker-gui.sh
@@ -7,7 +7,7 @@ WORKSPACE_DIR="$(pwd)"
 
 sudo xhost +si:localuser:root
 
-#xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge -
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge -
 
 sudo chmod 777 /tmp/.docker.xauth
 
@@ -16,7 +16,7 @@ XAUTH=/tmp/.docker.xauth
 sudo docker run --name test -it --privileged --network host \
     --shm-size=5G \
     -w /home/test \
-    -v $HOME/FENGSim:/home/test/FENGSim \
+    -v $WORKSPACE_DIR/..:/home/test/FENGSim \
     -e DISPLAY=$DISPLAY \
     -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
