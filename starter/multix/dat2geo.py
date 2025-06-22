da409d477b38332e88469810d56482b2aacbfc1e
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-06-22 10:37:41 +0800

    c

--- starter/multix/dat2geo.py
+++ starter/multix/dat2geo.py
@@ -4,78 +4,79 @@ import json
 import re
 import linecache
 
-geo_file_name = input("请输入geo网格文件名称：");
-geo_file_name = "Maxwell/conf/geo/" + geo_file_name + ".geo"
-print ("您输入的geo网格文件名称是: ", geo_file_name)
-
 dat_file_name = input("请输入dat网格文件名称：");
 dat_file_name = dat_file_name + ".dat"
 print ("您输入的dat网格文件名称是: ", dat_file_name)
 
-f1 = open(geo_file_name, 'r')
-f2 = open(dat_file_name, 'w')
+geo_file_name = input("请输入geo网格文件名称：");
+geo_file_name = "Maxwell/conf/geo/" + geo_file_name + ".geo"
+print ("您输入的geo网格文件名称是: ", geo_file_name)
 
-lines = f1.readlines()
-print(len(lines))
-
-ifnodal = 0
+f1 = open(dat_file_name, 'r')
+f2 = open(geo_file_name, 'w')
 
 n = 0
 m = 0
 
-parts = {}
+vertices = []
+elements = []
 
-for line in lines :
+line = f1.readline()
+line = line[:-1]
+values_point = re.split(' ',line)
+
+n = int(values_point[2])
+m = int(values_point[3])
+
+print(n)
+print(m)
+
+for i in range(0,n) :
+    line = f1.readline()
     line = line[:-1]
     values_point = re.split(' ',line)
-    if values_point[0]=="POINTS" :
-        ifnodal = 1
-    elif values_point[0]=="CELLS" :
-        ifnodal = 0
-    else :
-        if ifnodal == 1 :
-            n = n+1
-        else :
-            m = m+1
-            parts[values_point[1]] = "0"
+    vertices.append(float(values_point[0]))
+    vertices.append(float(values_point[1]))
+    vertices.append(float(values_point[2]))
 
-print(parts)
-
-f2.write("3 4 " + str(n) + " " + str(m)  + "\n")
-
-for line in lines :
+for i in range(0,m) :
+    line = f1.readline()
     line = line[:-1]
     values_point = re.split(' ',line)
-    if values_point[0]=="POINTS" :
-        ifnodal = 1
-        l = 0
-    elif values_point[0]=="CELLS" :
-        ifnodal = 0
-        l = 0
+    elements.append(0)
+    elements.append(int(values_point[0]))
+    elements.append(int(values_point[1]))
+    elements.append(int(values_point[2]))
+    elements.append(int(values_point[3]))
+
+f1 = open(dat_file_name, 'r')
+lines = f1.readlines()
+num = n+m+1
+i = 0
+check = 0
+sum = -1
+for line in lines :
+    if int(i) < int(num) :
+        i = i + 1
+        if check == 1 :
+            line = line[:-1]
+            values_point = re.split(' ',line)
+            elements[(int(values_point[0])-1)*5] = int(sum)
     else :
-        if ifnodal == 1 :
-            l = l + 1
-            f2.write(str(l) + " " + values_point[0] + " " + values_point[1] + " " + values_point[2] + "\n")
-        else :
-            l = l + 1
-            f2.write(str(l) + " " + str(int(values_point[2])+1) + " " + str(int(values_point[3])+1) + " " + str(int(values_point[4])+1) + " " + str(int(values_point[5])+1) + "\n")
-            parts[values_point[1]] = str(int(parts[values_point[1]])+1)
-
-print(parts)
-
-
-for key,values in parts.items():
-    f2.write(str(8) + " part_" + str(key) + " " + values + "\n")
-    l = 0
-    for line in lines :
         line = line[:-1]
         values_point = re.split(' ',line)
-        if values_point[0]=="POINTS" :
-            ifnodal = 1
-        elif values_point[0]=="CELLS" :
-            ifnodal = 0
-        else :
-            if ifnodal == 0 :
-                l = l + 1
-                if values_point[1] == key :
-                    f2.write(str(l) + "\n")
+        num = values_point[2]
+        i = 0
+        check = 1
+        print(num)
+        sum = sum + 1
+
+f2.write("POINTS\n")
+for i in range(0,n) :
+    f2.write(str(vertices[i*3]) + " " + str(vertices[i*3+1]) + " " + str(vertices[i*3+2]) + "\n")
+
+f2.write("CELLS\n")
+for i in range(0,m) :
+    f2.write(str(4) + " " + str(elements[i*5]) + " " + str(elements[i*5+1]) + " " + str(elements[i*5+2]) + " " + str(elements[i*5+3]) + " " + str(elements[i*5+4]) + "\n")
+
+    
