533a87d24cad51a3f98b2372c8620585d729db0f
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-08-21 15:01:35 +0800

    c

--- starter/mbdyn/robot/discrete.py
+++ starter/mbdyn/robot/discrete.py
@@ -1,4 +1,7 @@
 import re
+from scipy.spatial import distance
+import numpy as np
+import math
 
 fun_name = input("输入1为测试例子，输入2为读入ur3.traj：\n");
 print ("您的输入是: ", fun_name)
@@ -30,14 +33,36 @@ if int(fun_name) == 1:
         k += 1
     print("done")
 else :
-    f1 = open('ur3.traj', 'r')
-    f2 = open('ur3.pnts', 'w')
     with open('ur3.traj', 'r', encoding='utf-8') as file:
         lines = file.readlines()
+    f2 = open('ur3.pnts', 'w')
+    t = 0.2
+    p = 0.01
     i = 1
     while i < len(lines):
         line = lines[i][:-1]
         values_point = re.split(' ',line)
-        print(len(values_point))
+        
+        a = np.array([round(float(values_point[0]),10),round(float(values_point[1]),10),round(float(values_point[2]),10)])
+        b = np.array([round(float(values_point[3]),10),round(float(values_point[4]),10),round(float(values_point[5]),10)])
+        c = np.array([round(float(values_point[6]),10),round(float(values_point[7]),10),round(float(values_point[8]),10)])
+        v = round(float(values_point[9]),10)
+        d = round(np.linalg.norm(a-b),10)
+        n = (b-a) / d
+        l = v*p
+        m = math.floor(d/l)
+
+        j = 0
+        while j < m:
+            t += p
+            s = a+n*l*(j+1)
+            f2.write(str(t) + " " + str(s[0]) + " " + str(s[1]) + " "+ str(s[2]) + "\n")
+            j += 1
+
+        if m*l < d:
+            t += (d-m*l)/v
+            s = b
+            f2.write(str(t) + " " + str(s[0]) + " " + str(s[1]) + " "+ str(s[2]) + "\n")
+
         i += 1
     
