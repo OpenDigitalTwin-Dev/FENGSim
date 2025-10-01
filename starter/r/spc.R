c3b885c3d2692973e5b7e0809a38eff38aebbd47
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-10-01 22:49:18 +0800

    c

--- starter/r/spc.R
+++ starter/r/spc.R
@@ -1,2 +1,9 @@
-
-tfile <- "output.svg";svg(width=6,height=6,pointsize=10,filename=tfile); library(qcc,lib.loc='/home/jiping/FENGSim/toolkit/PS/install/r_install/lib/R/library');mu = 100;sigma_W = 10;epsilon = rnorm(500);x = matrix(mu + sigma_W*epsilon, ncol=10, byrow=TRUE);q = qcc(x, type="xbar");dev.off()
+tfile <- "output.svg"
+png("output.png",width=800,height=800,res=150,pointsize=12)
+library(qcc)
+mu = 100
+sigma_W = 10
+epsilon = rnorm(500)
+x = matrix(mu + sigma_W*epsilon, ncol=10, byrow=TRUE)
+q = qcc(x, type="xbar")
+dev.off()
